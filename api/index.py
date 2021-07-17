720c6c60fcabe871951cfaca19ce0340139d2d64
rikka <1103670381@qq.com>
2021-07-17 16:23:59 +0800

    Update: 前后端分离

--- api/index.py
+++ api/index.py
@@ -1,10 +1,14 @@
 # -*- coding:utf8 -*-
+# 这里的路由也是需要写上 /api 的
+
 import os
 from datetime import timedelta
-from flask import Flask, jsonify, request,session
+from flask import Flask, jsonify, request, session
 import pymongo
 import json
 import time
+
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
@@ -17,17 +21,28 @@ mycol = mydb["test"]
 
 @app.route('/api/login', methods=['post'])
 def login():
+    if session.get('username'):
+        return jsonify({'code': 1, 'msg': "已登录"})
     try:
         data = json.loads(request.get_data(as_text=True))
-        myquery = {'name': data['name'], 'password': data['password']}
+        myquery = {'username': data['username'], 'password': data['password']}
     except:
-        return "表单填写错误"
+        return jsonify({'code': 0, 'msg': "表单填写错误"})
     if len(list(mydb['config'].find(myquery))) != 0:
-        session['username'] = data['name']
+        session['username'] = data['username']
         session.permanent = True
-        return "登陆成功"
+        return jsonify({'code': 1, 'msg': "登陆成功"})
     else:
-        return "密码错误"
+        return jsonify({'code': 0, 'msg': "密码错误"})
+
+
+@app.route('/api/logout', methods=['get'])
+def logout():
+    if session.get('username'):
+        session['username'] = False
+        return jsonify({'code': 1, 'msg': "退出登录"})
+    else:
+        return jsonify({'code': 0, 'msg': "未登录"})
 
 
 # 查询, q 为查询条数
@@ -42,12 +57,13 @@ def query(q):
 # 返回最新创建的
 @app.route('/api/create', methods=['post'])
 def create():
-    new_content = json.loads(request.get_data(as_text=True))
-    new_content['date'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    new_content['_id'] = str(mycol.insert_one(new_content).inserted_id)
-    return jsonify(new_content)
-
-
+    if session.get("username"):
+        new_content = json.loads(request.get_data(as_text=True))
+        new_content['date'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        new_content['_id'] = str(mycol.insert_one(new_content).inserted_id)
+        return jsonify(new_content)
+    else:
+        return redirect("/static/login.html")
 
 
 if __name__ == '__main__':
